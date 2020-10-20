30b61702a253a17ea0e8818e7d9179a58cc65233
FarlenUreña <>
2020-10-20 16:23:31 -0600

    Se da inicio a la cración de la interfaz
    Se añaden clases necesarias junto con el login de la aplicacion.

---
+++ AeroInterfaz/src/main/java/org/una/aerointerfaz/App.java
@@ -0,0 +1,30 @@
+package org.una.aerointerfaz;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import org.una.aerointerfaz.utils.FlowController;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+       FlowController.getInstance().InitializeFlow(stage, null);
+       stage.setTitle("Gestión de Trámites Municipales");   
+       FlowController.getInstance().goViewInWindow("LoginView");
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
